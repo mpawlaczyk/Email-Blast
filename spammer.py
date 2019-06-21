1446fa29e40152a2657851c4019ab5ec677800a0
mpawlaczyk <pawlacmt@gmail.com>
2019-06-21 19:10:47 -0400

    creds and initial server connection

--- spammer.py
+++ spammer.py
@@ -1,13 +1,19 @@
 import smtplib, ssl
 
+# credential setup from config file
 email = ""
 passwd = ""
-
 with open("config.txt") as cfg_file:
     for line in cfg_file:
         if "email" in line:
             email = line.split(":")[1].strip(" ")
-            print email
         if "passwd" in line:
             passwd = line.split(":")[1].strip(" ")
-            print passwd
+
+# mail server setup
+port = 465
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(smtp.gmail.com, port, context=context) as mail_server:
+    mail_server.login(email, passwd)
+    
